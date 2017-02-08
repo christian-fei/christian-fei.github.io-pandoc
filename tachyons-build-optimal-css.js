6e70ebd1befa2b5913ddc64f6d0749dc79fff0fd
Christian Fei <crifei93@gmail.com>
2017-02-08 21:49:35 +0100

    update layout

--- tachyons-build-optimal-css.js
+++ tachyons-build-optimal-css.js
@@ -8,7 +8,7 @@ build(input, {
   from: tachyonsFileLocation,
   to: 'tachyons-build-optimal.css',
   minify: true,
-  whitelist: ['pl3','ml3','mh1','mh2','mh3','mh4','mh5','mh6','mh2','f4','f5','f6','link','white','black','ph3','intro-line','avenir','mb3','mb6','mb3','measure','fw6','mt0','f2','f-subheadline-l','ph5-ns','pv3','bg-black-10','mv3','w4', 'w2', 'mw4'],
+  whitelist: ['mw8','measure-wide','pl3','ml3','mh1','mh2','mh3','mh4','mh5','mh6','mh2','f4','f5','f6','link','white','black','ph3','intro-line','avenir','mb3','mb6','mb3','measure','fw6','mt0','f2','f-subheadline-l','ph5-ns','pv3','bg-black-10','mv3','w4', 'w2', 'mw4'],
   stripComments: true
 }).then(result => {
   fs.writeFileSync('./assets/css/tachyons-build-optimal.css', result.css)
