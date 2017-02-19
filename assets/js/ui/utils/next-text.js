59d968094f865b55b80fc14bbdca7195b4fd2acf
Christian Fei <crifei93@gmail.com>
2017-02-19 16:46:45 +0100

    update next-text

--- assets/js/ui/utils/next-text.js
+++ assets/js/ui/utils/next-text.js
@@ -1,6 +1,6 @@
 module.exports = function nextText (currentText, text) {
-    var currentLength = currentText.length
-    var remainingText = text.slice(currentLength)
-    var nextLetter = remainingText.length === 0 ? '' : remainingText[0]
-    return currentText + nextLetter
+  var currentLength = currentText.length
+  var remainingText = text.slice(currentLength)
+  var nextLetter = remainingText.length === 0 ? '' : remainingText[0]
+  return currentText + nextLetter
 }
