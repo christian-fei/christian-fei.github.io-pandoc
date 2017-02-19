ce80503c0c57cebd95630ad4c8933baffa1208cf
Christian Fei <crifei93@gmail.com>
2017-02-19 11:28:29 +0100

    fix concurrency between intervals

--- assets/js/ui/blog-search.js
+++ assets/js/ui/blog-search.js
@@ -1,8 +1,36 @@
 require('simple-jekyll-search')
 
+var searchInput = document.getElementById('search-input')
+
 window.SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
+  searchInput: searchInput,
   resultsContainer: document.getElementById('results-container'),
   json: '/search.json',
   searchResultTemplate: '<li><a class="pa" href="{url}">{title}</a></li>'
 })
+
+var nextText = require('./utils/next-text')
+var typingText = 'Type / to search...'
+var updateTextInterval
+
+searchInput.setAttribute('placeholder', '')
+setTimeout(startTyping, 1666)
+searchInput.addEventListener('blur', startTyping)
+searchInput.addEventListener('focus', function () {
+  clearInterval(updateTextInterval)
+})
+
+function startTyping () {
+  searchInput.setAttribute('placeholder', '')
+  updateWithNextText()
+  updateTextInterval = setInterval(updateWithNextText, 80)
+}
+
+function updateWithNextText () {
+  var currentPlaceholder = searchInput.getAttribute('placeholder')
+  var nextPlaceholder = nextText(currentPlaceholder, typingText)
+  if (nextPlaceholder === typingText) {
+    clearInterval(updateTextInterval)
+  }
+  searchInput.setAttribute('placeholder', nextPlaceholder)
+}
--- assets/js/ui/typing-search.js
+++ assets/js/ui/typing-search.js
@@ -1,29 +1,22 @@
+var nextText = require('./utils/next-text')
 var searchInput = document.getElementById('search-input')
-var searchBlinkTimer = 0
+var searchBlinkInterval
 var typingText = 'Type a keyword here...'
-var slashToSearchText = 'Type / to search'
-
-searchInput.setAttribute('placeholder', slashToSearchText)
 
 searchInput.addEventListener('blur', function () {
-  clearInterval(searchBlinkTimer)
-  searchInput.setAttribute('placeholder', slashToSearchText)
+  clearInterval(searchBlinkInterval)
 })
 searchInput.addEventListener('focus', function () {
   searchInput.setAttribute('placeholder', '')
   updateWithNextText()
-  searchBlinkTimer = setInterval(updateWithNextText, 70)
+  searchBlinkInterval = setInterval(updateWithNextText, 60)
 })
 
-function nextText (currentText) {
-  var currentLength = currentText.length
-  var remainingText = typingText.slice(currentLength)
-  if (remainingText.length === 0) {
-    clearInterval(searchBlinkTimer)
-  }
-  var nextLetter = remainingText.length === 0 ? '' : remainingText[0]
-  return currentText + nextLetter
-}
 function updateWithNextText () {
-  searchInput.setAttribute('placeholder', nextText(searchInput.getAttribute('placeholder')))
+  var currentPlaceholder = searchInput.getAttribute('placeholder')
+  var nextPlaceholder = nextText(currentPlaceholder, typingText)
+  if (nextPlaceholder === typingText) {
+    clearInterval(searchBlinkInterval)
+  }
+  searchInput.setAttribute('placeholder', nextPlaceholder)
 }
