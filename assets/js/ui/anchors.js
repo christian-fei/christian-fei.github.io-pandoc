9044dce9f35ac9acc8dc771f980be11d2af27e3d
Christian Fei <crifei93@gmail.com>
2018-04-12 22:34:35 +0200

    anchor left

--- assets/js/ui/anchors.js
+++ assets/js/ui/anchors.js
@@ -1,7 +1,7 @@
 var AnchorJS = require('anchor-js')
 var anchors = new AnchorJS()
 
-anchors.options.placement = 'right'
+anchors.options.placement = 'left'
 anchors.options.icon = '#'
 anchors.options.visible = 'no'
 anchors.add('h1, h2, h3, h4, h5, h6')
