0a498d6147ab41f16d696f8ad0002ba96e9ab47d
Christian Fei <crifei93@gmail.com>
2018-06-12 21:48:13 +0200

    cleanup

--- assets/js/ui/header-scroll-hide.js
+++ assets/js/ui/header-scroll-hide.js
@@ -1,12 +1,5 @@
 var navbar = document.querySelector('.navbar.is-fixed-top')
 
-// console.log('navbar', navbar)
-// console.log('window.scrollY', window.scrollY)
-
-// let lastCheck = Date.now()
-// console.log('lastCheck', lastCheck)
-// let lastScrollY
-
 if (window.scrollY > 250) {
   navbar.style.display = 'none'
 }
@@ -14,11 +7,9 @@ if (window.scrollY > 250) {
 window.addEventListener('scroll', function () {
   if (window.lastY < window.scrollY) {
     navbar.style.display = 'none'
-    // navbar.classList.add('hide')
   }
   if (window.lastY > window.scrollY) {
     navbar.style.display = 'inherit'
-    // navbar.classList.remove('hide')
   }
   window.lastY = window.scrollY
 })
